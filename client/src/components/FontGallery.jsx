9467173ce93509b76c44d63cca9874da6ea30810
Hyeji1364 <161557112+Hyeji1364@users.noreply.github.com>
2024-07-29 09:55:01 +0900

    upload

--- client/src/components/FontGallery.jsx
+++ client/src/components/FontGallery.jsx
@@ -5,12 +5,19 @@ import "../assets/scss/fontgallery.scss";
 const FontGallery = () => {
   const [fonts, setFonts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태 추가
 
   useEffect(() => {
     fetch("/assets/data/koreanfont.json")
       .then((response) => response.json())
-      .then((data) => setFonts(data))
-      .catch((error) => console.error("Error loading font data:", error));
+      .then((data) => {
+        setFonts(data);
+        setIsLoading(false); // 폰트 데이터 로드 완료 후 로딩 상태 해제
+      })
+      .catch((error) => {
+        console.error("Error loading font data:", error);
+        setIsLoading(false); // 오류 발생 시에도 로딩 상태 해제
+      });
   }, []);
 
   const handleSearchChange = (event) => {
@@ -34,11 +41,15 @@ const FontGallery = () => {
           onChange={handleSearchChange}
         />
       </div>
-      <div className="font-wrap">
-        {filteredFonts.map((font, index) => (
-          <FontCard key={index} font={font} />
-        ))}
-      </div>
+      {isLoading ? ( // 로딩 중일 때 표시할 내용
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="font-wrap">
+          {filteredFonts.map((font, index) => (
+            <FontCard key={index} font={font} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
